f8a53f3009120c5b36d84d51878cc6ddae19d2ef
Andy Bae <sjandybae@gmail.com>
2020-03-05 11:34:11 -0800

    Switch to PHP, WILL BREAK THE SITE

--- projects/forlorn.php
+++ projects/forlorn.php
@@ -19,7 +19,7 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Projects</a>
+                        <a class="nav-link" href="projects">Projects</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
---
+++ projects/index.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>Andy Bae | Narrative Designer</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../css/site.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <!--Header-->
+        <nav class="navbar navbar-expand-md navbar-light sticky-top">
+            <a class="navbar-brand" href="/">
+                <h1>Andy Bae</h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Explore</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">My resume</a>
+                            <a class="dropdown-item" href="#">LinkedIn</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+        <!--THE MAIN CONTENT OF THIS PAGE-->
+        <div id="main">
+            PROJECTS DIRECTORY
+        </div>
+        <!--END OF MAIN CONTENT-->
+        
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6"><p>To contact me, please go to my <a href="https://ca.linkedin.com/in/andy-bae-20ba97b1" target="_blank">LinkedIn</a>.</p></div>
+                    <div class="col-sm-6"><small>Copyright &copy; Andy Bae 2020</small></div>
+                </div>
+            </div>
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="../js/site.js"></script>
+    </body>
+</html>
