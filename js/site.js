3809212f49567d9633e1c9213b14f4ad2e04ce18
Andy Bae <sjandybae@gmail.com>
2020-03-10 17:38:45 -0700

    Added font and style to homepage

--- js/site.js
+++ js/site.js
@@ -1,6 +1,19 @@
 $( document ).ready(function() {
-    //console.log( "hi!" );
+    console.log( "hi!" );
+    $("#creativeBio").hide();
+    $("#technicalBio").hide();
     
     //$("#main").append("<div>jquery was here</div>")
     
+    $("#homepageCreativeButton").click(function() {
+        $("#technicalBio").hide();
+        $("#creativeBio").show();
+        $('html, body').animate({
+            scrollTop: $("#creativeBio").offset().top
+        }, 500);
+    });
+    $("#homepageTechnicalButton").click(function() {
+        $("#creativeBio").hide();
+        $("#technicalBio").show();
+    });
 });
